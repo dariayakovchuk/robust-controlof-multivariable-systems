c8427157ae5bc82bdfadfc5d83d0af5ada9fcf82
daria <daria.y.k.29@gmail.com>
2022-08-09 14:13:20 +0200

    Changes: optimization

--- design.py
+++ design.py
@@ -16,15 +16,15 @@ def plant():
     """
     plant() -> G - LTI object
     """
-    num = [[[0, 2.]]]
-    den = [[[1, -.9]]]
-    sys1 = tf(num, den)
+    num = [0, 2.]
+    den = [1, -.9]
+    sys1 = tf(num, den, Ts)
     return ss(sys1)
 
 
 def synth_h2(g, ejw, w, w1, w2):
     n = 3
-    x, y, gamma = cp.Variable((n,1)), cp.Variable((n,1)), cp.Variable((len(w), 1), nonneg=True)
+    x, y, gamma = cp.Variable((n, 1)), cp.Variable((n, 1)), cp.Variable((len(w), 1), nonneg=True)
     GAMMA_prev = 0.
     cost = 0
     constraints, K = [], []
@@ -36,17 +36,15 @@ def synth_h2(g, ejw, w, w1, w2):
         Xc, Yc = 0.1 * (ejw[i]**2), (ejw[i]**2)
         P, Pc = Y + G * X, Yc + G * Xc
         T = conj(P) * Pc + conj(Pc) * P - conj(Pc) * Pc
-        # f = cp.hstack([w1*Y, w2*X])
-        F = w1*Y
+        f = cp.hstack([w1*Y, w2*X])
         I = np.eye(2)
-        tmp = cp.vstack([cp.hstack([GAMMA, F]), cp.hstack([conj(F), T])])
-        # tmp2 = cp.vstack([cp.hstack([I*Z, f]), cp.hstack([conj(f), T])])
-        # print(t[i] - t[i-1])
-        if i==0:
-            cost = GAMMA/2*w[0]
+        # tmp = cp.vstack([cp.hstack([GAMMA, w1*Y]), cp.hstack([conj(w1*Y), T])])
+        tmp = cp.vstack([cp.hstack([(I*GAMMA)[0], f[0]]), cp.hstack([(I*GAMMA)[1], f[1]]), cp.hstack([conj(f), T])])
+        if i == 0:
+            cost += (GAMMA * w[0]) / 2
         else:
             cost += ((GAMMA_prev + GAMMA) / 2) * (w[i] - w[i-1])
-        constraints += [ cp.bmat([[GAMMA, F],[ conj(F), T]]) >> 0]
+        constraints += [tmp >> 0]
         GAMMA_prev = GAMMA
     obj = cp.Minimize(2 * cost)
     prob = cp.Problem(obj, constraints)
@@ -72,7 +70,7 @@ def design():
     """Show results of designs"""
     W1 = 1.
     W2 = 1.
-    t = np.logspace(np.log10(0.01), np.log10(math.pi/Ts), 40) # (a,b) 10^a ..... 10 ^b   -> c ... d ---> a = log(c), b = log(d)
+    t = np.logspace(np.log10(0.01), np.log10(math.pi/Ts), 400) # (a,b) 10^a ..... 10 ^b   -> c ... d ---> a = log(c), b = log(d)
     H2_perf(t, W1, W2)
 
 if __name__ == "__main__":
