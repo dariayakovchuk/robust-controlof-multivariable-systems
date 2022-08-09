36c2affec324f8acae3a6039a9890f07b33b55be
daria <daria.y.k.29@gmail.com>
2022-08-09 16:58:16 +0200

    Changes: optimization for MIMO

--- design.py
+++ design.py
@@ -18,7 +18,9 @@ def plant():
     """
     num = [0, 2.]
     den = [1, -.9]
-    sys1 = tf(num, den, Ts)
+    # sys1 = tf(num, den, Ts)  # SISO system
+    sys1 = tf([[[n * 2 for n in num], [n * 0.1 for n in num]], [[n * 0 for n in num], num]],
+              [[den, den], [den, den]], Ts)  # MIMO system
     return ss(sys1)
 
 
@@ -27,19 +29,23 @@ def synth_h2(g, ejw, w, w1, w2):
     x, y, gamma = cp.Variable((n, 1)), cp.Variable((n, 1)), cp.Variable((len(w), 1), nonneg=True)
     GAMMA_prev = 0.
     cost = 0
-    constraints, K = [], []
+    constraints = []
     for i in range(len(w)):
         G = g[i]
-        X = x[0] * (ejw[i]**2) + x[1] * ejw[i] + x[2]
-        Y = 1 * (ejw[i]**2) + y[1] * ejw[i] + y[2]
+        # X = x[0] * (ejw[i]**2) + x[1] * ejw[i] + x[2]
+        # Y = 1 * (ejw[i]**2) + y[1] * ejw[i] + y[2]
+        X = cp.bmat([[x[0] * (ejw[i] ** 2) + x[1] * ejw[i] + x[2], 0], [0, x[0] * (ejw[i] ** 2) + x[1] * ejw[i] + x[2]]])
+        Y = cp.bmat([[1 * (ejw[i] ** 2) + y[1] * ejw[i] + y[2], 0], [0, 1 * (ejw[i] ** 2) + y[1] * ejw[i] + y[2]]])
         GAMMA = gamma[i]
-        Xc, Yc = 0.1 * (ejw[i]**2), (ejw[i]**2)
-        P, Pc = Y + G * X, Yc + G * Xc
+        Xc, Yc = 0.1 * (ejw[i]**2), ejw[i]**2
+        # P, Pc = Y + G * X, Yc + G * Xc
+        P, Pc = Y + G @ X, Yc + G * Xc
         T = conj(P) * Pc + conj(Pc) * P - conj(Pc) * Pc
         f = cp.hstack([w1*Y, w2*X])
-        I = np.eye(2)
+        I = np.eye(4)
         # tmp = cp.vstack([cp.hstack([GAMMA, w1*Y]), cp.hstack([conj(w1*Y), T])])
-        tmp = cp.vstack([cp.hstack([(I*GAMMA)[0], f[0]]), cp.hstack([(I*GAMMA)[1], f[1]]), cp.hstack([conj(f), T])])
+        # tmp = cp.vstack([cp.hstack([(I*GAMMA)[0], f[0]]), cp.hstack([(I*GAMMA)[1], f[1]]), cp.hstack([conj(f), T])])
+        tmp = cp.vstack([cp.hstack([(I*GAMMA), f.T]), cp.hstack([conj(f), T])])
         if i == 0:
             cost += (GAMMA * w[0]) / 2
         else:
@@ -57,7 +63,8 @@ def freq_response(g, w):
     mag, phase, omega = freqresp(g, w)
     # print('mag, phase, omega', mag, phase, omega)
     ejw = np.exp(1j * w * Ts)
-    sjw = mag * np.exp(1j * phase)
+    # sjw = mag * np.exp(1j * phase)
+    sjw = (mag * np.exp(1j * phase)).transpose(2, 0, 1)
     return sjw, ejw
 
 def H2_perf(t, w1, w2):
@@ -70,7 +77,7 @@ def design():
     """Show results of designs"""
     W1 = 1.
     W2 = 1.
-    t = np.logspace(np.log10(0.01), np.log10(math.pi/Ts), 400) # (a,b) 10^a ..... 10 ^b   -> c ... d ---> a = log(c), b = log(d)
+    t = np.logspace(np.log10(0.01), np.log10(math.pi/Ts), 40) # (a,b) 10^a ..... 10 ^b   -> c ... d ---> a = log(c), b = log(d)
     H2_perf(t, W1, W2)
 
 if __name__ == "__main__":
