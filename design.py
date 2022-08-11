5402410eeeae9776f04b9fbb603937b27e255c50
daria <daria.y.k.29@gmail.com>
2022-08-11 15:55:05 +0200

    mosek fusion

--- design.py
+++ design.py
@@ -1,5 +1,6 @@
 import math
 import os
+from mosek.fusion import *
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,53 +19,43 @@ def plant():
     """
     num = [0, 2.]
     den = [1, -.9]
-    # sys1 = tf(num, den, Ts)  # SISO system
     sys1 = tf([[[n * 2 for n in num], [n * 0.1 for n in num]], [[n * 0 for n in num], num]],
               [[den, den], [den, den]], Ts)  # MIMO system
     return ss(sys1)
 
 
 def synth_h2(g, ejw, w, w1, w2):
+    M = Model("semidefinite model")
     n = 3
-    x, y, gamma = cp.Variable((n, 1)), cp.Variable((n, 1)), cp.Variable((len(w), 1), nonneg=True)
+    x, y, gamma = M.variable([n, 1]), M.variable([n, 1]), M.variable([len(w), 1], Domain.greaterThan(0.))
     GAMMA_prev = 0.
     cost = 0
     constraints = []
     for i in range(len(w)):
         G = g[i]
-        # X = x[0] * (ejw[i]**2) + x[1] * ejw[i] + x[2]
-        # Y = 1 * (ejw[i]**2) + y[1] * ejw[i] + y[2]
-        X = cp.bmat([[x[0] * (ejw[i] ** 2) + x[1] * ejw[i] + x[2], 0], [0, x[0] * (ejw[i] ** 2) + x[1] * ejw[i] + x[2]]])
-        Y = cp.bmat([[1 * (ejw[i] ** 2) + y[1] * ejw[i] + y[2], 0], [0, 1 * (ejw[i] ** 2) + y[1] * ejw[i] + y[2]]])
+        X = Expr.outer(x.index([0, 0]), ejw[i] ** 2)
+        Y = [[Expr.mul(1 * (ejw[i] ** 2)) + y[1] * ejw[i] + y[2], 0], [0, 1 * (ejw[i] ** 2) + y[1] * ejw[i] + y[2]]]
         GAMMA = gamma[i]
         Xc, Yc = 0.1 * (ejw[i]**2), ejw[i]**2
-        # P, Pc = Y + G * X, Yc + G * Xc
         I2 = np.eye(2)
-        P, Pc = Y + G @ X, (I2*Yc) + G @ (I2*Xc)
-        T = P.H @ Pc + Pc.conjugate().transpose() @ P - Pc.conjugate().transpose() @ Pc
-        f = cp.vstack([w1*Y, w2*X])
+    #     P, Pc = Y + G @ X, (I2*Yc) + G @ (I2*Xc)
+    #     T = P.H @ Pc + Pc.conjugate().transpose() @ P - Pc.conjugate().transpose() @ Pc
+    #     f = cp.vstack([w1*Y, w2*X])
         I = np.eye(4)
-        # tmp = cp.vstack([cp.hstack([GAMMA, w1*Y]), cp.hstack([conj(w1*Y), T])])
-        # tmp = cp.vstack([cp.hstack([(I*GAMMA)[0], f[0]]), cp.hstack([(I*GAMMA)[1], f[1]]), cp.hstack([conj(f), T])])
-        tmp = cp.vstack([cp.hstack([(I*GAMMA), f]), cp.hstack([f.H, T])])
+    #     tmp = cp.vstack([cp.hstack([(I*GAMMA), f]), cp.hstack([f.H, T])])
         if i == 0:
             cost += (GAMMA * w[0]) / 2
         else:
             cost += ((GAMMA_prev + GAMMA) / 2) * (w[i] - w[i-1])
-        constraints += [tmp >> 0]
+        M.constraint((tmp, Domain.inPSDCone()))
         GAMMA_prev = GAMMA
-    obj = cp.Minimize(2 * cost)
-    prob = cp.Problem(obj, constraints)
-    prob.solve(solver=cp.MOSEK, verbose=True)
-    print("status:", prob.status)
-    print("optimal value", prob.value)
-    return x.value, y.value
+    M.objective(ObjectiveSense.Minimize, 2 * cost)    # obj = cp.Minimize(2 * cost)
+    M.solve()
+    # return x.value, y.value
 
 def freq_response(g, w):
     mag, phase, omega = freqresp(g, w)
-    # print('mag, phase, omega', mag, phase, omega)
     ejw = np.exp(1j * w * Ts)
-    # sjw = mag * np.exp(1j * phase)
     sjw = (mag * np.exp(1j * phase)).transpose(2, 0, 1)
     return sjw, ejw
 
@@ -81,6 +72,6 @@ def design():
     t = np.logspace(np.log10(0.01), np.log10(math.pi/Ts), 10) # (a,b) 10^a ..... 10 ^b   -> c ... d ---> a = log(c), b = log(d)
     H2_perf(t, W1, W2)
 
+
 if __name__ == "__main__":
-    # analysis()
     design()
